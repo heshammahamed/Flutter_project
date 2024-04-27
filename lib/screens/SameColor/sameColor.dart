c59689ddc1836cb2f7d1080eff8dfebf14dcf6e2
hesham mahamed <hesham.workk@gmail.com>
2024-04-27 15:15:57 +0300

    fix small yamel file

---
+++ lib/screens/SameColor/sameColor.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SameColor extends StatelessWidget {
+  const SameColor({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF161616) ,
+      body : Center(child : Text("code!!!!!"))
+    );
+  }
+}
