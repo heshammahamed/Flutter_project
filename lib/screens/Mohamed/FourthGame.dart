3024e3caf644898f1fa7d424e9f174f2a65b9ac0
unknown <mohammed511yasser@gmail.com>
2024-05-06 19:31:59 +0300

    add momamed game

--- lib/screens/Mohamed/FourthGame.dart
+++ lib/screens/Mohamed/FourthGame.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'dart:async';
+import 'dart:math';
 
 class MohamedGame extends StatefulWidget {
   const MohamedGame({super.key});
@@ -9,22 +10,48 @@ class MohamedGame extends StatefulWidget {
 }
 
 class _MohamedGameState extends State<MohamedGame> {
+  int score = 100; // عدد النقاط
+  Color mainColor = Colors.red[500]!; // اللون الأساسي
+  Color brightestColor = Colors.red[400]!; // اللون الأساسي المشرق
 
+  // مكان ظهور اللون المختلف
+  int differentColorIndex = 0;
 
-List mainColors = [Colors.red[500] , Colors.blue[500] , Colors.purple[500]];
-List brightestColors = [Colors.red[400] , Colors.blue[400] , Colors.purple[400]];
+  int timerValue = 120; // قيمة الوقت بالثواني
+  late Timer _timer; // تايمر
 
-var mainColor = Colors.red[500];
-var brightestColor = Colors.red[400];
+  @override
+  void initState() {
+    super.initState();
+    // بدء التايمر عند تهيئة الحالة
+    _startTimer();
+  }
+
+  // دالة لبدء التايمر
+  void _startTimer() {
+    const oneSec = const Duration(seconds: 1);
+    _timer = new Timer.periodic(
+      oneSec,
+      (timer) {
+        setState(() {
+          if (timerValue < 1) {
+            timer.cancel();
+          } else {
+            timerValue--;
+          }
+        });
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.green,
-        title: const Text(
-          "Timer : 2                 Score : 100",
-          style: TextStyle(
+        title: Text(
+          "different the degree color",
+          style: const TextStyle(
             color: Colors.white,
             fontSize: 30,
             fontWeight: FontWeight.w700,
@@ -33,42 +60,108 @@ var brightestColor = Colors.red[400];
       ),
       body: Center(
         child: Container(
+          padding: EdgeInsets.only(
+            top: 30,
+            right: 10,
+            left: 10,
+          ),
           width: 500,
-          height: 700,
+          height: 500,
           decoration: BoxDecoration(
             borderRadius: const BorderRadius.all(
                 Radius.circular(10)), //علشان اعمل حواف دائريه للكونتينر
             border: Border.all(color: Colors.black, width: 5), //البرواز الخارجى
           ),
-          child:
-          
-          GridView.builder(
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3, // Number of columns in the grid
-                crossAxisSpacing: 10.0, // Spacing between columns
-                mainAxisSpacing: 10.0, // Spacing between rows
-              ),
+          child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3, // Number of columns in the grid
+              crossAxisSpacing: 10.0, // Spacing between columns
+              mainAxisSpacing: 10.0, // Spacing between rows
+            ),
 
-  itemCount: 9, // Total number of items in the grid
-  itemBuilder: (BuildContext context, int index) {
-    // This function is called for every item in the grid
-    // It returns the widget to display for each item
-    return Container(
-          decoration: BoxDecoration(
-            color: mainColor,
-            borderRadius: const BorderRadius.all(Radius.circular(7)),
+            itemCount: 9, // Total number of items in the grid
+            itemBuilder: (BuildContext context, int index) {
+              // تحديد مكان ظهور اللون المختلف
+              if (index == differentColorIndex) {
+                return GestureDetector(
+                  onTap: () {
+                    // عندما يضغط المستخدم على اللون المختلف
+                    setState(() {
+                      // تحديث النقاط
+                      score += 10;
+                      // توليد درجة عشوائية قريبة للون الأساسي
+                      int randomIncrement = Random().nextInt(100);
+                      mainColor = Color.fromRGBO(
+                        mainColor.red + randomIncrement,
+                        mainColor.green + randomIncrement,
+                        mainColor.blue + randomIncrement,
+                        1,
+                      );
+                      brightestColor = Color.fromRGBO(
+                        brightestColor.red + randomIncrement,
+                        brightestColor.green + randomIncrement,
+                        brightestColor.blue + randomIncrement,
+                        1,
+                      );
+                      // توليد رقم عشوائي لتحديد مكان جديد للون المختلف
+                      differentColorIndex = Random().nextInt(9);
+                    });
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: brightestColor,
+                      borderRadius: const BorderRadius.all(Radius.circular(7)),
+                    ),
+                    width: 111,
+                    height: 370,
+                  ),
+                );
+              } else {
+                // This function is called for every item in the grid
+                // It returns the widget to display for each item
+                return GestureDetector(
+                  onTap: () {
+                    // عندما يضغط المستخدم على الألوان الأخرى
+                    setState(() {
+                      // تحديث النقاط
+                      score -= 5;
+                      // توليد درجة عشوائية جديدة للون الأساسي
+                      mainColor = Color.fromRGBO(
+                        Random().nextInt(256),
+                        Random().nextInt(256),
+                        Random().nextInt(256),
+                        1,
+                      );
+                      // تحديد اللون المشرق
+                      brightestColor = Color.fromRGBO(
+                        mainColor.red - 100,
+                        mainColor.green - 100,
+                        mainColor.blue - 100,
+                        1,
+                      );
+                    });
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: mainColor,
+                      borderRadius: const BorderRadius.all(Radius.circular(7)),
+                    ),
+                    width: 111,
+                    height: 370,
+                  ),
+                );
+              }
+            },
           ),
-          width: 111,
-          height: 370,
-        );
-  },
-),
+        ),
       ),
-    ));
+    );
+  }
+
+  @override
+  void dispose() {
+    // إلغاء التايمر عند تجميع الحالة
+    _timer.cancel();
+    super.dispose();
   }
 }
-
-
-
-
-
