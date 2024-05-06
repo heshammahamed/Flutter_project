1832127daefbb51c1865d97c2bdf755e7585128b
hesham mahamed <hesham.workk@gmail.com>
2024-05-06 22:23:20 +0300

    gh

--- lib/screens/FifthGame/FifthGame.dart
+++ lib/screens/FifthGame/FifthGame.dart
@@ -1,96 +1,52 @@
 import 'dart:async';
 import 'dart:math';
-import 'package:audioplayers/audioplayers.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 
 
-
-
-// features : 
-
-// problems : 
-// clean the code and add comments
-// the timer show 5 4 2 0 in the UI but print  5 4 3 2 1 0
-// add sound win the anser is correct and if it wrong 
-// change the UI because its very very bad
-
-
-
-// For change the state of the variables
-
-class Model extends ChangeNotifier {
-  int score = 0;
-  addOne () {
-    score = score + 500;
-    notifyListeners();
-  }
-
-  minOne () {
-    score = score - 1000;
-    notifyListeners();
-  }
-}
-
-
-
-
-
-// Main wedgit in the screen
-
-class FifthGame extends StatelessWidget {
+class FifthGame extends StatefulWidget {
   const FifthGame({super.key});
 
-    @override
-  Widget build(BuildContext context) {
-    return ChangeNotifierProvider(create: (context) => Model() , child :
-     Scaffold(
-      // back ground color for the screen
-      backgroundColor: const Color(0xFF161616),   
-      body : 
-      
-      Container (
-
-        margin: const EdgeInsets.symmetric(horizontal: 20 , vertical: 40),
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.white , width: 5),
-        ),
-
-
-        child: 
-        
-        Column (
-          children: [
-
-            SizedBox(height: MediaQuery.of(context).size.height * 0.09),
-            const BodyOfTheGame(),
-            const Head(),
-          ]
-        ) 
-
-      ) ,
-    ));
-  }
-}
-
-
-// Wedgit display the timer and score
-
-class Head extends StatefulWidget {
-  const Head({super.key});
-
   @override
-  State<Head> createState() => _HeadState();
+  State<FifthGame> createState() => _FifthGameState();
 }
 
-class _HeadState extends State<Head> {
+class _FifthGameState extends State<FifthGame> {
+
+
+// variables 
+
 
   int _secondsRemaining = 60;
   late Timer _timer;
+  int score = 0;
 
-  @override
+  String feedbackMessage = "";
+  Color feedbackColor = Colors.transparent; // Set default color
+  bool displayFeedback = false;
+  late Timer _feedbackTimer = Timer(Duration.zero, () {}); // Initialize with an empty timer
+
+
+
+
+  List<String> directionsList = ["Down","Up","Left", "Right"];
+  String swap = "right";  // wich direction the user swap
+  List iconDirections = [FontAwesomeIcons.arrowDown, FontAwesomeIcons.arrowUp , FontAwesomeIcons.arrowLeft, FontAwesomeIcons.arrowRight];
+  List arrowColors = [Colors.red, Colors.green, Colors.red, Colors.green, Colors.red];
+  String direction = "Right"; // the direction wich display in the screen
+  var iconDirection = FontAwesomeIcons.arrowDown;
+  String iconDirectionString = "Down";  // arrow direction 
+  var arrowColor = Colors.red;
+  bool directionSwapped = false;
+  int i = 0;  // used to pick up the same value from the iconDirections list and direction list
+
+  changeDirectionOfArrow (){
+   i = Random().nextInt(4);
+  }
+
+
+    @override
   void initState() {
     super.initState();
     _startTimer();
@@ -99,8 +55,10 @@ class _HeadState extends State<Head> {
   @override
   void dispose() {
     _timer.cancel();
+    _feedbackTimer.cancel(); // Cancel the feedback timer on dispose
     super.dispose();
   }
+
 
   void _startTimer() {
     _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
@@ -109,114 +67,102 @@ class _HeadState extends State<Head> {
           _secondsRemaining--;
         } else {
           timer.cancel();
-          // Timer completed
+          _showPopup();
         }
       });
     });
   }
 
-  
-  @override
-  Widget build(BuildContext context) {
+  void _startFeedbackTimer() {
+    setState(() {
+      displayFeedback = true;
+    });
 
-
-
-    return Container(
-      decoration : BoxDecoration (
-        color : Colors.blue,
-        border : Border.all(width : 2 , color : Colors.yellow)
-      ),
-      width : 250,
-      child : 
-         Row (
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Consumer<Model>(builder: (context, model, child) => 
-            Container(
-                child: Text ("Score : ${model.score}" , style: const TextStyle(color: Colors.white , fontSize: 25))
-                )
-        ,) ,
-
-        Row (
-          children : [
-            const FaIcon(FontAwesomeIcons.tiktok,color: Colors.red,size: 20,)
-            ,
-        // timer section
-        Container(
-          child: Text ("Timer : $_secondsRemaining" , style: const TextStyle(color: Colors.white , fontSize: 25))
-        ),
-          ]
-        )
-
-
-        // score section
-
-      ],
-    )
-    )
-    
- ;
+    _feedbackTimer = Timer(Duration(milliseconds: 1000), () {
+      setState(() {
+        displayFeedback = false;
+      });
+    });
   }
-}
+
+  void _showPopup() {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Time Is Over"),
+          content: Text("Your Score : $score"),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                // Navigate to the main menu
+                // You can replace '/home' with your main menu route
+                Navigator.pushNamed(context, '/');
+              },
+              child: const Text("Main Menu"),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                // Rebuild the current screen
+                setState(() {
+                  _secondsRemaining = 60; // Reset timer
+                  score = 0; // Reset Score
+                  _startTimer(); // Start timer again
+                });
+              },
+              child: const Text("Play Again"),
+            ),
+          ],
+        );
+      },
+    );
+  }
 
 
-
-// Wedgit wich display the arrow and the direction text 
-
-class BodyOfTheGame extends StatefulWidget {
-  const BodyOfTheGame({super.key});
-
-  @override
-  State<BodyOfTheGame> createState() => _BodyOfTheGameState();
-}
-
-class _BodyOfTheGameState extends State<BodyOfTheGame> {
-
-final audio = AudioPlayer();
-
-  // variables
-  List<String> directionsList = ["Down","Up","Left", "Right"];
-
-  
-  String swap = "right";  // wich direction the user swap
-
-
-  List iconDirections = [FontAwesomeIcons.arrowDown, FontAwesomeIcons.arrowUp , FontAwesomeIcons.arrowLeft, FontAwesomeIcons.arrowRight];
-  List arrowColors = [Colors.red, Colors.green, Colors.red, Colors.green, Colors.red];
-
-  String direction = "Right"; // the direction wich display in the screen
-
-  var iconDirection = FontAwesomeIcons.arrowDown;
-  String iconDirectionString = "Down";  // arrow direction 
-  var arrowColor = Colors.red;
-
-  bool directionSwapped = false;
-
-
-int i = 0;  // used to pick up the same value from the iconDirections list and direction list
-
-changeDirectionOfArrow (){
-  i = Random().nextInt(4);
-}
-
-  @override
-  Widget build(BuildContext context) {
-  var model = Provider.of<Model>(context);
 
 checkSwap() {
   if (!directionSwapped) {
     
     if (arrowColor == Colors.green) {
       if (iconDirectionString == swap) {
-        model.addOne();
+        setState(() {
+          score = score + 500;
+          feedbackMessage = "Correct +100";
+          feedbackColor = Colors.green;
+          displayFeedback = true;
+        });
       }else {
-        model.minOne();
+        setState(() {
+          score = score - 1000;
+          feedbackMessage = "Wrong -1000";
+          feedbackColor = Colors.red;
+          displayFeedback = true;
+          if (score < 0) {
+            score = 0;
+          }
+        });
       }
     }else {
     if (direction == swap) {
-      model.addOne();
+      setState(() {
+        score = score + 500;
+        feedbackMessage = "Correct +100";
+        feedbackColor = Colors.green;
+        displayFeedback = true;
+      });
     } else {
-      model.minOne();
+      setState(() {
+        score = score - 1000;
+        feedbackMessage = "Wrong -40";
+        feedbackColor = Colors.red;
+        displayFeedback = true;
+        if (score < 0) {
+          score = 0;
+        }
+      });
       }
     }
 
@@ -230,87 +176,147 @@ changeDirectionOfArrow();
     });
   }
 }
-    return GestureDetector (
-
-      onVerticalDragUpdate: (details) {
-      int sensitivity = 8;
-
-        if (details.delta.dy > sensitivity) {
-          swap = "Down";
-          directionSwapped = false;
-        }
-
-        if (details.delta.dy < -sensitivity) {
-          swap = "Up";
-          directionSwapped = false;
-      }},
 
 
-      onHorizontalDragUpdate :(details) {
-        int sensitivity = 8;
-
-        if (details.delta.dx > sensitivity) {
-          swap = "Right";
-          directionSwapped = false;
-        }
-        
-        if (details.delta.dx < -sensitivity) {
-          swap = "Left";
-          directionSwapped = false;
-      }},
-
-
-    onVerticalDragEnd: (details) {
-    Timer(const Duration(milliseconds: 200), () {
-      checkSwap();
-    });
-  },
-  onHorizontalDragEnd: (details) {
-    Timer(const Duration(milliseconds: 200), () {
-      checkSwap();
-    });
-  },
-
-    child: 
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // back ground color for the screen
+      backgroundColor: const Color(0xFF161616),   
+      body : 
+      
+    
     Container (
 
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.red , width: 2),
-      ),
+        margin: const EdgeInsets.symmetric(horizontal: 20 , vertical: 40),
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.white , width: 5),
+        ),
 
-      height: MediaQuery.of(context).size.height * 0.6 ,
+        child: 
+        Column (
+          children: [
 
-      padding: const EdgeInsets.symmetric(horizontal: 40),
+                      // first child in the column
+                      Container(
+                                decoration : BoxDecoration (
+                                                color : Colors.blue,
+                                                border : Border.all(width : 2 , color : Colors.yellow)
+                                              ),
+                                width : 250,
+                                child :
+
+                                    Row (
+                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                      children: [
+
+                                        // Timer section 
+
+                                        Container(
+                                          child: Text ("Timer : $_secondsRemaining" , style: const TextStyle(color: Colors.white , fontSize: 25))
+                                        ),
+
+                                        // Score section
+
+                                       Container(
+                                                child: Text ("Score : ${score}" , style: const TextStyle(color: Colors.white , fontSize: 25))
+                                        ),
+                                      ]
       
-      child: 
-      Column (
-        children : [
-
-          // show swap text and direction text
-          Container (
-
-            decoration: const BoxDecoration(
-              color : Colors.red,
-            ),
-
-            child :        
-            Row (
-            children: [
-            const Text ("Swap : " , style: TextStyle(color: Colors.yellow , fontSize: 40)),
-            Text (direction , style: const TextStyle(color: Colors.white , fontSize: 35)),
-            ],
-          ),
-          ),
+                                    )
+                        ),
 
 
-          SizedBox(height: MediaQuery.of(context).size.height * 0.2),
 
-          // show the arrow icon
+                      // space between the first and second child in column
+                      SizedBox(height: MediaQuery.of(context).size.height * 0.09),
 
-          Center (child: FaIcon(iconDirection,color: arrowColor,size: 190.0,))
-      ]
-    )));
-  }
-}
+
+
+                     // second element in the tree
+                     GestureDetector (
+
+
+                      onVerticalDragUpdate: (details) {
+                      int sensitivity = 8;
+
+                      if (details.delta.dy > sensitivity) {
+                        swap = "Down";
+                        directionSwapped = false;
+                      } 
+
+                     if (details.delta.dy < -sensitivity) {
+                      swap = "Up";
+                      directionSwapped = false;
+                    }},
+
+
+                    onHorizontalDragUpdate :(details) {
+                      int sensitivity = 8;
+
+                      if (details.delta.dx > sensitivity) {
+                        swap = "Right";
+                        directionSwapped = false;
+                      }
+        
+                      if (details.delta.dx < -sensitivity) {
+                        swap = "Left";
+                        directionSwapped = false;
+                    }},
+
+
+                    onVerticalDragEnd: (details) {
+                      Timer(const Duration(milliseconds: 200), () {
+                        checkSwap();
+                      });
+                    },
+                    onHorizontalDragEnd: (details) {
+                      Timer(const Duration(milliseconds: 200), () {
+                        checkSwap();
+                      });
+                    },
+
+
+
+                    
+                    child: 
+                     Container (
+
+                        decoration: BoxDecoration(
+                                 border: Border.all(color: Colors.red , width: 2),
+                        ),
+
+                        height: MediaQuery.of(context).size.height * 0.6 ,
+                        padding: const EdgeInsets.symmetric(horizontal: 40),
+      
+                      child: 
+                            Column (
+                                    children : [
+
+                                      // show swap text and direction text
+                                      Container (
+
+                                        decoration: const BoxDecoration(
+                                          color : Colors.red,
+                                        ),
+
+                                        child :        
+                                         Row (
+                                         children: [
+                                         const Text ("Swap : " , style: TextStyle(color: Colors.yellow , fontSize: 40)),
+                                         Text (direction , style: const TextStyle(color: Colors.white , fontSize: 35)),
+                                         ],
+                                       ),),
+
+
+                                      SizedBox(height: MediaQuery.of(context).size.height * 0.2),
+                                        // show the arrow icon
+
+                                      Center (child: FaIcon(iconDirection,color: arrowColor,size: 190.0,))
+                                    ]))),]) ) ,);}}
+
+
+
 
 
--- lib/screens/SameColor/sameColor.dart
+++ lib/screens/SameColor/sameColor.dart
@@ -13,25 +13,180 @@ import 'dart:async';
 // add sound win the anser is correct and if it wrong 
 // change the UI because its very very bad
 
-class Model extends ChangeNotifier {
-  int score = 0;
-  addOne () {
-    score = score + 500;
-    notifyListeners();
-  }
 
-  minOne () {
-    score = score - 1000;
-    notifyListeners();
-  }
-}
 
-class SameColor extends StatelessWidget {
+
+
+
+
+
+
+
+
+
+
+
+
+
+class SameColor extends StatefulWidget {
   const SameColor({super.key});
 
   @override
+  State<SameColor> createState() => _SameColorState();
+}
+
+class _SameColorState extends State<SameColor> {
+
+int score = 0;
+int _secondsRemaining = 60;
+late Timer _timer;
+
+String feedbackMessage = "";
+Color feedbackColor = Colors.transparent; // Set default color
+bool displayFeedback = false;
+late Timer _feedbackTimer = Timer(Duration.zero, () {}); // Initialize with an empty timer
+
+
+
+List<String> colors = ["Red" , "Blue" , "Black" , "Purple"];
+
+String meaining = "Red";
+String text = "Green";
+
+List textColors = [Colors.red , Colors.blue , Colors.black , Colors.purple];
+var textColor = Colors.red;
+String textColorString = "Red";
+
+int i = 0;  // used to pick up the same value from the iconDirections list and direction list
+
+changeDirectionOfArrow (){
+  i = Random().nextInt(4);
+}
+
+
+    @override
+  void initState() {
+    super.initState();
+    _startTimer();
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    _feedbackTimer.cancel(); // Cancel the feedback timer on dispose
+    super.dispose();
+  }
+
+
+  void _startTimer() {
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        if (_secondsRemaining > 0) {
+          _secondsRemaining--;
+        } else {
+          timer.cancel();
+          _showPopup();
+        }
+      });
+    });
+  }
+
+void _startFeedbackTimer() {
+    setState(() {
+      displayFeedback = true;
+    });
+
+    _feedbackTimer = Timer(Duration(milliseconds: 1000), () {
+      setState(() {
+        displayFeedback = false;
+      });
+    });
+}
+
+void _showPopup() {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Time Is Over"),
+          content: Text("Your Score : $score"),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                // Navigate to the main menu
+                // You can replace '/home' with your main menu route
+                Navigator.pushNamed(context, '/');
+              },
+              child: const Text("Main Menu"),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                // Rebuild the current screen
+                setState(() {
+                  _secondsRemaining = 60; // Reset timer
+                  score = 0; // Reset Score
+                  _startTimer(); // Start timer again
+                });
+              },
+              child: const Text("Play Again"),
+            ),
+          ],
+        );
+      },
+    );
+}
+
+
+  changeState () {
+    setState(() {
+      meaining = colors[Random().nextInt(4)];
+      text = colors[Random().nextInt(4)];
+      textColor = textColors[i];
+      textColorString = colors[i];      
+    });
+  }
+
+  trueCheckScore () {
+    if (meaining == textColorString) {
+      setState(() {
+      score = score + 500; 
+      feedbackMessage = "Correct +100";
+      feedbackColor = Colors.green;
+      displayFeedback = true;       
+      });
+    }else {
+      setState(() {
+      score = score - 1000;        
+      });
+    }
+    changeDirectionOfArrow();
+    changeState();
+  }
+
+  falseCheckScore () {
+    if (meaining != textColorString) {
+      setState(() {
+      score = score + 500;   
+      feedbackMessage = "Correct +100";
+      feedbackColor = Colors.green;
+      displayFeedback = true;     
+      });
+    }else {
+      setState(() {
+      score = score - 1000;        
+      });
+    }
+    changeDirectionOfArrow();
+    changeState();
+  }
+
+
+  @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(create: (context) => Model() , child :
+    return 
     Scaffold(
       backgroundColor: const Color(0xFF161616) ,
       body : 
@@ -48,14 +203,178 @@ class SameColor extends StatelessWidget {
         
         Column (
           children: [
-            const Head(),
-            SizedBox(height: MediaQuery.of(context).size.height * 0.09),
-            const BodyOfTheGAme()
+    Row (
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+
+        // timer section
+        Container(
+          child: Text ("Timer : $_secondsRemaining" , style: const TextStyle(color: Colors.white , fontSize: 25))
+        ),
+
+        // score section
+        Container(
+                child: Text ("Score : ${score}" , style: const TextStyle(color: Colors.white , fontSize: 25))
+        )
+      ],),
+
+
+
+      SizedBox(height: MediaQuery.of(context).size.height * 0.09),
+
+
+
+Container (
+
+      child: Column (
+        children: [
+
+          const Text("Is the meaning at the top the same as the color at the bottom?" , style : TextStyle(color: Colors.white , fontSize: 17 , fontWeight: FontWeight.w500)),
+
+          const SizedBox(height: 70,),
+
+          Column (
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+
+                width: 150,
+                height: 50,
+                alignment: Alignment.center,
+                child : const Text("meaning" , style: TextStyle(fontSize: 20 , fontWeight: FontWeight.bold),),
+              ),
+              
+              const SizedBox(height: 20),
+              Container (
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+
+                width: 300,
+                height: 100,
+                alignment: Alignment.center,
+                child: 
+                Text(meaining , style: const TextStyle(fontSize: 50)),
+              )
+            ],
+          ),
+
+          const SizedBox(height: 25),
+
+          Column (
+            children: [             
+
+              Container (
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+
+                width: 300,
+                height: 100,
+                alignment: Alignment.center,
+                child: 
+                Text(text , style: TextStyle(color : textColor ,fontSize: 50)),
+              ),
+              const SizedBox(height: 20),
+
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+
+                width: 150,
+                height: 50,
+                alignment: Alignment.center,
+                child : const Text("color" , style: TextStyle(fontSize: 20 , fontWeight: FontWeight.bold),),
+              ),
+            ],
+          ),
+
+          const SizedBox(height: 45),
+
+          Row (
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+
+              GestureDetector(
+
+                onTap:() => trueCheckScore(),
+
+                child : 
+              
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.amber,
+                  borderRadius: BorderRadius.circular(40)
+                ),
+                alignment: Alignment.center,
+                width: 150,
+                height: 70,
+
+                child: const Text("True" , style: TextStyle(fontSize: 40 , fontWeight: FontWeight.w400),)
+              )),
+
+
+              GestureDetector(
+
+                onTap: () => falseCheckScore(),
+
+                child : 
+              
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.amber,
+                  borderRadius: BorderRadius.circular(40)
+                ),
+                alignment: Alignment.center,
+                width: 150,
+                height: 70,
+
+                child: const Text("False" , style: TextStyle(fontSize: 40 , fontWeight: FontWeight.w400),)
+              )),
+            ],
+          )
+        ],
+      ))
           ]
         ) 
 
       ) 
-    ));
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Model extends ChangeNotifier {
+  int score = 0;
+  addOne () {
+    score = score + 500;
+    notifyListeners();
+  }
+
+  minOne () {
+    score = score - 1000;
+    notifyListeners();
   }
 }
 
@@ -103,7 +422,8 @@ class _HeadState extends State<Head> {
 
 
 
-    return Row (
+    return 
+    Row (
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
 
@@ -174,7 +494,8 @@ class _BodyOfTheGAmeState extends State<BodyOfTheGAme> {
     changeState();
   }
 
-    return Container (
+    return
+     Container (
 
       child: Column (
         children: [
